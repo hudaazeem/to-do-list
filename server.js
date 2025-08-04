96b15251ff77433ba39b92dfb51b6ed650f92a60
hudaazeem <145403156+hudaazeem@users.noreply.github.com>
2025-08-04 15:44:09 +0000

    finished basic backend and frontend of website

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 const express = require('express');
-
-
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 
-// hello world example
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static('express-locallibrary-tutorial/public'));
+
+let todos =[];
+
+app.get('/api/todos', (req, res) => {
+    res.json(todos);
 })  
 
+app.post('/api/todos', (req, res) => {
+    const newTodo = {
+        id: Date.now(),
+        text: req.body.text,
+        done: false
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
 
-// Start the server
+app.put('/api/todos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const todo = todos.find(t => t.id === id);
+    if (!todo) return res.status(404).json({error:"Not found"});
+
+    todo.text = req.body.text !== undefined ? req.body.text : todo.text;
+    todo.done = req.body.done !== undefined ? req.body.done : todo.done;
+    res.json(todo);
+});
+
+
+
+app.delete('/api/todos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    todos = todos.filter(t => t.id !== id);
+    res.json({message: "Deleted"});
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
