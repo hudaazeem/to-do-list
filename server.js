198bb4432ef85031a5408eb1af31e441a4a0ed37
hudaazeem <145403156+hudaazeem@users.noreply.github.com>
2025-08-07 16:45:46 +0000

    fixing deployment issue (5)

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.use((err, req, res, next) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
